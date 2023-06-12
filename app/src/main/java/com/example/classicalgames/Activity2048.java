2b00290bf00bdeec058f7ffddff813d06b257332
ENE <hoang20021103@gmail.com>
2023-06-12 22:06:08 +0700

    2048 layout

---
+++ app/src/main/java/com/example/classicalgames/Activity2048.java
@@ -0,0 +1,15 @@
+package com.example.classicalgames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2048 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2048);
+
+    }
+}
